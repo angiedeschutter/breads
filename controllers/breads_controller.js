20033ebbf3d245aaf7129dfac707b4390673db08
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-19 18:30:16 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,19 +5,29 @@ const Bread = require('../models/bread.js')
 
 
 // SHOW
+// SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
-  
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
+
 // INDEX
 breads.get('/', (req, res) => {
   res.render('Index',
     {
-      breads: Bread
+      breads: Bread,
+      title: 'Index Page'
     }
   )
-// res.send(Bread)
 })
+
 
 
 
