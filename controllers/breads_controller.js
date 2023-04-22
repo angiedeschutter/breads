a0ec2b6c0bee6ce6b945b43fcc3121a3c1d74e11
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-22 10:07:09 -0700

    delet route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,13 +11,13 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.render('error404')
+    res.render('404')
   }
 })
-
 
 // INDEX
 breads.get('/', (req, res) => {
@@ -43,6 +43,11 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 
 
